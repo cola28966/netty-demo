c4903891644584976118e779f95425100b326851
yangfangmin <fangmin.yang@ximalaya.com>
2023-03-19 17:42:38 +0800

    聊天室登录

---
+++ netty-demo/src/main/java/netty/im/client/ChatClient.java
@@ -0,0 +1,79 @@
+package netty.im.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+import netty.im.message.LoginRequestMessage;
+import netty.im.protocol.MessageCodec;
+import netty.im.protocol.MessageCodecSharable;
+import netty.im.protocol.ProcotolFrameDecoder;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Slf4j
+public class ChatClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        try{
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(group);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast("client handler", new ChannelInboundHandlerAdapter(){
+
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            log.debug("msg:{}", msg);
+                        }
+
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            // 会在连接 chanel 建立成功后，会触发 active 事件
+                            new Thread(()->{
+                                Scanner scanner = new Scanner(System.in);
+                                System.out.println("请输入用户名：");
+                                String username = scanner.nextLine();
+                                System.out.println("请输入密码：");
+                                String password = scanner.nextLine();
+                                // 构造消息对象
+                                LoginRequestMessage message = new LoginRequestMessage(username, password);
+                                // 发送消息
+                                ctx.writeAndFlush(message);
+
+                                System.out.println("等待后续操作：");
+                                try {
+                                    System.in.read();
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }, "system in").start();
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
